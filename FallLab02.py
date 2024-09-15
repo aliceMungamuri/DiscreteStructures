4839cb53f4db62d2f0bef09706fd84cd53687761
aliceMungamuri <158488797+aliceMungamuri@users.noreply.github.com>
2024-09-15 15:00:11 -0500

    Update FallLab02.py

--- FallLab02.py
+++ FallLab02.py
@@ -5,8 +5,14 @@
 # Description : Write a program that takes input from a list of [0,7] and makes pairs from the output [A,H], from the pairs determines if its a function and if it is if it is a one-to-one, onto, or both
 # Collaborators: None
 
-input = input('Enter your input pairs in the form\n (4,F)(5,G)(4,H)\n The inputs are a list of mappings of elements from set [0,1,2,3,4,5,6,7] to the elements of set [A,B,C,D,E,F,G,H]\n 1 ≤ Number of pairs in input ≤ 16\n')
+input = input('Enter your input pairs in the form\n (4,F)(5,G)(4,H)\n The inputs are a list of mappings of elements from set [0,1,2,3,4,5,6,7] to the elements of set [A,B,C,D,E,F,G,H]\n 1 ≤ Number of pairs in input ≤ 16\n Do NOT put the same pair in: ex. (1,A)(B,2)(1,A)')
 print(input)
+
+function = True
+one-to-one = True
+onto = True
+checkOntoList = [A,B,C,D,E,F,G,H]
+
 
 splitInput = input.split(')(')
 
@@ -14,8 +20,20 @@ inputList = []
 outputList = []
 
 for i in splitInput:
-  in, out = i.split(',')
-  inputList.append(in)
+  in_, out = i.split(',')
+  inputList.append(in_)
   outputList.append(out)
-print(inputList)
-print(outputList)
+
+# Checking if multiple inputs
+for i in inputLIst:
+  if inputList.count(i) > 1:
+    function = False
+    print ('not function')
+# checking if function is onto
+if function == True:
+  for i in checkOntoList:
+    if i not in outputLIst:
+      onto = False
+
+# Req = one to one 2 inputs cant go to one output, onto means all outputs covered not a function if an input goes to 2 outputs
+  
