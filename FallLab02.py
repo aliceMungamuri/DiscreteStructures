2ed73404bd1f1a16b5a6de6711c19b453f460038
aliceMungamuri <158488797+aliceMungamuri@users.noreply.github.com>
2024-09-15 15:20:35 -0500

    Update FallLab02.py

--- FallLab02.py
+++ FallLab02.py
@@ -5,7 +5,7 @@
 # Description : Write a program that takes input from a list of [0,7] and makes pairs from the output [A,H], from the pairs determines if its a function and if it is if it is a one-to-one, onto, or both
 # Collaborators: None
 
-input = input('Enter your input pairs in the form\n (4,F)(5,G)(4,H)\n The inputs are a list of mappings of elements from set [0,1,2,3,4,5,6,7] to the elements of set [A,B,C,D,E,F,G,H]\n 1 ≤ Number of pairs in input ≤ 16\n Do NOT put the same pair in: ex. (1,A)(B,2)(1,A)')
+input = input('Enter your input pairs in the form\n (4,F)(5,G)(4,H)\n The inputs are a list of mappings of elements from set [0,1,2,3,4,5,6,7] to the elements of set [A,B,C,D,E,F,G,H]\n 1 ≤ Number of pairs in input ≤ 16\n Do NOT put the same pair in: ex. (1,A)(B,2)(1,A)\n')
 print(input)
 
 function = True
