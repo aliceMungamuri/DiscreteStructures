9c02628139fd70384e507dcf27f3ef703eb09cec
aliceMungamuri <158488797+aliceMungamuri@users.noreply.github.com>
2024-09-22 14:51:08 -0500

    Update FallLab03.py

--- FallLab03.py
+++ FallLab03.py
@@ -1,1 +1,22 @@
+#Name: Alice Mungamuri
+# KUID: 3117704
+# Lab Session Monday 8am
+#LAB Assignment 03
+# Description : Write a program that implements the algorithm for fast modular exponentiation 
+# Collaborators: None
+ # 
+b = input ('What is your base\n1 ≤ b ≤ 100')
+n = input ('What is your exponent\n 1 ≤ n ≤ 500')
+m = input ('What is your remainder\n1 ≤ m ≤ 500.')
 
+res = 0
+# base of just b mod m
+base = b%m
+# need to keep on going until exponent is o 
+if n>0:
+  
+
+
+
+
+    
