3208f765b8b2d55d0d22302036f006902442caad
aliceMungamuri <158488797+aliceMungamuri@users.noreply.github.com>
2024-09-22 15:11:32 -0500

    Update FallLab03.py

--- FallLab03.py
+++ FallLab03.py
@@ -19,7 +19,7 @@ while n>0:
   res = (res*base)%m
  n = n//2 # move to next high bit --> makes it smaller
  base = (base*base)%m # taking mmod m of the square
- print(f"{str(b)}^{str(ogN)} mod {str(m)} = {str(res)}")
+print(f"{str(b)}^{str(ogN)} mod {str(m)} = {str(res)}")
   
 
     
