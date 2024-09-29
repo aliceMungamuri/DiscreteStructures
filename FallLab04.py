b5bcce2a7fd867ba7689c035e6f970b006596daa
aliceMungamuri <158488797+aliceMungamuri@users.noreply.github.com>
2024-09-29 18:11:35 -0500

    Update FallLab04.py

--- FallLab04.py
+++ FallLab04.py
@@ -7,9 +7,21 @@
 x = int(input('What number are you searching for'))
 
 listn = []
-firstItem = 0
-lastItem = len(listn)
-middleSpot = (firstItem + lastItem)//2
-
-if len(listn) == 1 and listn[0] != x: # this is if x isn't in the list
-  print('0')
+def search(listn, x):
+  firstItem = 0
+  lastItem = len(listn)
+  middleSpot = (firstItem + lastItem)//2
+  if len(listn) == 1 and listn[0] != x: # this is if x isn't in the list
+    print('0')
+  if  listn[middleSpot] == x:
+    return middleSpot
+  elif  listn[middleSpot] < x:
+    listn = listn[middleSpot:lastItem]
+    newSearch = search(listn,x)
+        # if v is not False, add it to the left side of the window and return
+        # else return False
+        return newSearch + middleSpot if newSearch != False else newSearch
+    elif listn[middleSpot] > x:
+        listn = listn[firstItem:middleSpot]
+        newSearch = search(listn,x)
+        return newSearch + firstItem if newSearch != False else newSearch
