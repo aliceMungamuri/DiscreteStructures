ebc2e7887478476c25f65acc741979357061a89f
aliceMungamuri <158488797+aliceMungamuri@users.noreply.github.com>
2024-10-02 12:35:10 -0500

    Update FallLab05.py

--- FallLab05.py
+++ FallLab05.py
@@ -1,1 +1,13 @@
-F
+list1 = [] # the lists
+list2 = []
+
+lengthList1 = len(list1) # i dont think these are necessary
+lengthList2 = len(list2)
+i = 0
+j = 0
+
+def mergeList(list1,list2):
+  L = [] # new list
+  if list1[0]<= list2[0]:
+    L.append([list1])
+  
