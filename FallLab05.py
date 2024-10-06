031703b5891309d5b4a6e03052eade1289bc8070
aliceMungamuri <158488797+aliceMungamuri@users.noreply.github.com>
2024-10-06 00:04:49 -0500

    Update FallLab05.py

--- FallLab05.py
+++ FallLab05.py
@@ -1,25 +1,30 @@
-# Name: Alice Mungamuri
+#Name: Alice Mungamuri
 # KUID: 3117704
 # Lab Session Monday 8am
-# LAB Assignment 05
-# Description: Merge two ordered lists of positive integers
+#LAB Assignment 05
+# Description : Merge Sort of a list
 # Collaborators: NONE
-
-list1 = []  # the lists
+list1 = [] # the lists
 list2 = []
+
+lengthList1 = len(list1) # i dont think these are necessary
+lengthList2 = len(list2)
+i = 0
+j = 0
 
 # for setting the lists up
 n = int(input('Enter the number of elements for list 1: '))
-for i in range(0, n):
-    element = int(input('Put your element (remember in increasing order and 1 ≤ n ≤ 32, 1 ≤ m ≤ 32): '))
-    list1.append(element)
-
+for i in range(0,n):
+  element = int(input('Put your element (remember in increasing order and 1 ≤ n ≤ 32, 1 ≤ m ≤ 32 )'))
+  list1.append(element)
 n = int(input('Enter the number of elements for list 2: '))
-for i in range(0, n):
-    element = int(input('Put your element (remember in increasing order and 1 ≤ n ≤ 32, 1 ≤ m ≤ 32): '))
-    list2.append(element)
+for i in range(0,n):
+  element = int(input('Put your element (remember in increasing order and 1 ≤ n ≤ 32, 1 ≤ m ≤ 32 )'))
+  list2.append(element)
 
 def mergeList(list1, list2):
+    L = []  # new list
+    
     # Base cases for recursion
     if not list1:  # If list1 is empty
         return list2  # Return list2
@@ -27,18 +32,19 @@ def mergeList(list1, list2):
         return list1  # Return list1
 
     # Recursive merging logic
-    if list1[0] < list2[0]:
-        # Include the first element of list1 and recurse
-        return [list1[0]] + mergeList(list1[1:], list2) if list1[0] not in list2 else mergeList(list1[1:], list2)
-    elif list1[0] > list2[0]:
-        # Include the first element of list2 and recurse
-        return [list2[0]] + mergeList(list1, list2[1:]) if list2[0] not in list1 else mergeList(list1, list2[1:])
-    else:
-        # Both elements are equal; skip one and recurse
-        return mergeList(list1[1:], list2[1:])
+    if list1[0] < list2[0]:  # If the first element of list1 is smaller
+        L.append(list1[0])  # Add the first element of list1
+        L += mergeList(list1[1:], list2)  # Recur with the rest of list1
+    elif list1[0] > list2[0]:  # If the first element of list2 is smaller
+        L.append(list2[0])  # Add the first element of list2
+        L += mergeList(list1, list2[1:])  # Recur with the rest of list2
+    else:  # They are equal
+        L.append(list1[0])  # Add one of the equal elements (from either list)
+        L += mergeList(list1[1:], list2[1:])  # Recur with the rest of both lists
 
-# Print the merged list
-result = mergeList(list1, list2)
-# Removing duplicates from the final output
-result = list(dict.fromkeys(result))  # Maintain order while removing duplicates
-print(result)
+    return L
+print(mergeList(list1,list2))
+
+
+  
+  
