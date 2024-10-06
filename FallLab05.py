340ffae5b6a005766a688f34cdd1a4e262226373
aliceMungamuri <158488797+aliceMungamuri@users.noreply.github.com>
2024-10-05 23:28:49 -0500

    Update FallLab05.py

--- FallLab05.py
+++ FallLab05.py
@@ -1,48 +1,43 @@
-#Name: Alice Mungamuri
+# Name: Alice Mungamuri
 # KUID: 3117704
-# Lab Session Monday 8am
-#LAB Assignment 05
-# Description : Merge Sort of a list
-# Collaborators: NONE
-list1 = [] # the lists
-list2 = []
+# Lab Session: Monday 8am
+# Lab Assignment 05
+# Description: Recursive Binary Search to find the position of an integer in an ordered list
 
-lengthList1 = len(list1) # i dont think these are necessary
-lengthList2 = len(list2)
-i = 0
-j = 0
-
-# for setting the lists up
-n = int(input('Enter the number of elements for list 1: '))
-for i in range(0,n):
-  element = int(input('Put your element (remember in increasing order and 1 ≤ n ≤ 32, 1 ≤ m ≤ 32 )'))
-  list1.append(element)
-n = int(input('Enter the number of elements for list 2: '))
-for i in range(0,n):
-  element = int(input('Put your element (remember in increasing order and 1 ≤ n ≤ 32, 1 ≤ m ≤ 32 )'))
-  list2.append(element)
-
-def mergeList(list1,list2):
-  L = [] # new list
-  if list1 == [] and list2 == []:
-    return L
-  if list1 != [] and list2 == []:
-    L = L.append(list1)
-  if list1 == [] and list2 != []:
-    L = L.append(list2)
+# Function to perform recursive binary search
+def recursive_binary_search(lst, target, low, high):
+    # Base case: target is not found
+    if low > high:
+        return 0
     
+    # Find the middle index
+    mid = (low + high) // 2
     
-  if list1[0]<= list2[0]:
-    L.append(list1[0])
-    L = L + mergeList(list1[1:],list2)
-  if list2[0]<= list1[0]:
-    L.append(list2[0])
-    L = L + mergeList(list1,list2[1:])
-  setL = set(L) # for getting rid of the duplicates
-  L = list(setL)
-  
-  return L
+    # Check if the middle element is the target
+    if lst[mid] == target:
+        return mid + 1  # Convert to 1-based index
+    
+    # If target is smaller, search the left half
+    elif target < lst[mid]:
+        return recursive_binary_search(lst, target, low, mid - 1)
+    
+    # If target is larger, search the right half
+    else:
+        return recursive_binary_search(lst, target, mid + 1, high)
 
+# Input section
+n = int(input('Enter the number of elements for the list: '))
+lst = []
 
-  
-  
+# Collect elements of the list
+for i in range(n):
+    element = int(input(f'Enter element {i+1} (in increasing order): '))
+    lst.append(element)
+
+# Input the target integer
+target = int(input('Enter the target integer to search for: '))
+
+# Call the binary search function
+position = recursive_binary_search(lst, target, 0, n - 1)
+
+print(position)
