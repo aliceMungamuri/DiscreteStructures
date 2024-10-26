27a2aafef556b2588e92df466178cb11cdf53c07
aliceMungamuri <158488797+aliceMungamuri@users.noreply.github.com>
2024-10-26 17:38:42 -0500

    Update FallLab07.py

--- FallLab07.py
+++ FallLab07.py
@@ -5,17 +5,17 @@
 # Description : Output of Bit Sequences
 # Collaborators: NONE
 
-def makeSequence(n, current_sequence="", lastNum="", output=""):
+def makeSequence(n, current_sequence="", lastNum="", newString=""):
     if len(current_sequence) == n:
         output += current_sequence + ", "
-        return output  # Return the updated output string
+        return newString  #returns the new string
     
-    # Generate sequences by adding "1" and, if applicable, "0"
+    # new sequences by adding 1 and 0
     if lastNum != "0":
-        output = makeSequence(n, current_sequence + "0", "0", output)
-    output = makeSequence(n, current_sequence + "1", "1", output)
+        newString = makeSequence(n, current_sequence + "0", "0", newString)
+        newString = makeSequence(n, current_sequence + "1", "1", newString)
     
-    return output
+    return newString
 
 def main():
     n = int(input("Enter an integer (1 <= n <= 32): "))
@@ -24,8 +24,8 @@ def main():
         print("Output for n=1: 0, 1")
         return
     
-    print(f"Output for integer={n}: ", end="")
-    output = makeSequence(n)
-    print(output[:-2])  # Slice off the last comma and space
+    print(f"Output for {n}: ", end="")
+    newString = makeSequence(n)
+    print(newString[:-2])  # Slice off the last comma and space bc I couldn't figure out how to do it inside the recursive function
 
 main()
