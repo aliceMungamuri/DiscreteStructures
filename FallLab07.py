1104f9c081665c4e3317836ff680c3406a815c68
aliceMungamuri <158488797+aliceMungamuri@users.noreply.github.com>
2024-10-26 11:15:12 -0500

    Update FallLab07.py

--- FallLab07.py
+++ FallLab07.py
@@ -2,28 +2,27 @@
 # KUID: 3117704
 # Lab Session Monday 8am
 #LAB Assignment 06
-# Description : Next Permutation
+# Description : Output of Bit Sequences
 # Collaborators: NONE
-
-def generate_sequences(lengthSequences, current_sequence="", lastNum=""):
-    if len(current_sequence) == lengthSequences:
+def makeSequence(n, current_sequence="", lastNum=""):
+    if len(current_sequence) == n:
         print(current_sequence, end=", ")
         return
     
     # Add 1 to the current sequence
-    generate_sequences(lengthSequences, current_sequence + "1", "1")
+    makeSequence(n, current_sequence + "1", "1")
     
     # Add 0 to the current sequence only if the last digit is not 0
     if lastNum != "0":
-        generate_sequences(lengthSequences, current_sequence + "0", "0")
+        makeSequence(n, current_sequence + "0", "0")
 
 def main():
-    lengthSequences = int(input("Enter an integer (1 ≤ n ≤ 32): "))
-  # Handle base case for n = 1
+    n = int(input("Enter an integer (1 <= n <= 32: "))
+# this is about handling the base case if n = 1
     if n == 1:
         print("Output for n=1: 0, 1, ", end="")
         return
-    print(f"Output for integer={lengthSequences}: ", end="")
-    generate_sequences(n)
+    print(f"Output for integer={n}: ", end="")
+    makeSequence(n)
 
 main()
