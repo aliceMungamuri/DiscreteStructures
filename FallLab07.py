b82a68d4dfafdffbad33ca0847500c4a61d08637
aliceMungamuri <158488797+aliceMungamuri@users.noreply.github.com>
2024-10-27 00:37:03 -0500

    Update FallLab07.py

--- FallLab07.py
+++ FallLab07.py
@@ -5,27 +5,27 @@
 # Description : Output of Bit Sequences
 # Collaborators: NONE
 
-def makeSequence(n, current_sequence="", lastNum="", newString=""):
+def makeSequence(n, current_sequence="", lastNum="", output=""):
     if len(current_sequence) == n:
-        newString += current_sequence + ", "
-        return newString  #returns the new string
+        output += current_sequence + ", "
+        return output  # this gives back the new and updates string
     
-    # new sequences by adding 1 and 0
+    # makes the sequences by adding 1 and if it works, 0
     if lastNum != "0":
-        newString = makeSequence(n, current_sequence + "0", "0", newString)
-        newString = makeSequence(n, current_sequence + "1", "1", newString)
+        output = makeSequence(n, current_sequence + "0", "0", output)
+    output = makeSequence(n, current_sequence + "1", "1", output)
     
-    return newString
+    return output
 
 def main():
     n = int(input("Enter an integer (1 <= n <= 32): "))
     
-    if n == 1:
+    if n == 1: # this would be the base case when n = 1
         print("Output for n=1: 0, 1")
         return
     
     print(f"Output for integer={n}: ", end="")
-    newString = makeSequence(n)
-    print(newString[:-2])  # Slice off the last comma and space bc I couldn't figure out how to do it inside the recursive function
+    output = makeSequence(n)
+    print(output[:-2])  # slice off the last comma at the very end, outside of the recursion because I dont know how to do it inside the recursive method effectively
 
 main()
